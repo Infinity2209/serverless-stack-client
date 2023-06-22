5177cc70e242b8cf5b8b5ba2e9da32c16e427d8f
Infinity2209 <anantanag91@gmail.com>
2023-06-22 09:16:14 +0530

    Building our React app

--- src/components/UnauthenticatedRoute.js
+++ src/components/UnauthenticatedRoute.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { Route, useNavigate } from "react-router-dom";
 import { useAppContext } from "../libs/contextLib";
-
-export default function UnauthenticatedRoute({ children, ...rest }) {
-  const { isAuthenticated } = useAppContext();
-  const navigate = useNavigate();
-
-  if (isAuthenticated) {
-    navigate("/");
+function querystring(name, url = window.location.href) {
+  name = name.replace(/[[]]/g, "\\$&");
+  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i");
+  const results = regex.exec(url);
+  if (!results) {
     return null;
   }
-
-  return <Route {...rest} element={children} />;
+  if (!results[2]) {
+    return "";
+  }
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+export default function UnauthenticatedRoute({ children, ...rest }) {
+  const { isAuthenticated } = useAppContext();
+  const redirect = querystring("redirect");
+  const navigate=useNavigate();
+  return (
+    <Route {...rest}>
+      {!isAuthenticated ? (
+        children
+      ) : (
+        <navigate to={redirect === "" || redirect === null ? "/" : redirect} />
+      )}
+    </Route>
+  );
 }

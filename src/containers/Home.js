ea6e4f56a6f82a684ae658e8e37fe2398c4b18f8
Infinity2209 <anantanag91@gmail.com>
2023-06-25 22:37:46 +0530

    Adding invalid method

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -26,7 +26,7 @@ export default function Home() {
     onLoad();
   }, [isAuthenticated]);
   function loadNotes() {
-    return API.get("notes", "/invalid_path");
+    return API.get("notes", "/notes");
   }
 
   function renderNotesList(notes) {
